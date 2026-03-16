01ce3323c6ddf5c456a8b70d3df1970a3b0d1886
Hwanee <khhh0508@gmail.com>
2026-03-16 20:55:07 +0900

    day2 공부내용

---
+++ week1/Day2_print()_advance.py
@@ -0,0 +1,27 @@
+print("안녕","하세요.","반갑습니다.")
+print("안녕하세요.", "반갑습니다.")
+print("나이:", 20, "살입니다.")
+
+AGE=20
+print("나이:",AGE,"살입니다.")
+print("나이:"+str(AGE)+" 살입니다.")
+print("나이:",str(AGE+10),"살입니다.")
+print("나이:",str(AGE+20),"살입니다.")
+print("숫자를 선택하세요.", "1.", 20, "2.", 30, "3.", 40)
+print("내 나이 :", AGE)
+print("내 나이 :", AGE+10)
+print("내 나이 :", AGE+20)
+print("내 나이 :"+str(AGE))
+
+print(str(100))
+
+score=100
+print(str(score))
+print("점수:",str(score)+"점입니다.")
+
+this_year=2026
+print("올해는", this_year,"년입니다.")
+print("올해는", str(this_year)+"년입니다."
+      "내년은", str(this_year+1)+"년입니다."
+      "작년은", str(this_year-1)+"년입니다."
+)
