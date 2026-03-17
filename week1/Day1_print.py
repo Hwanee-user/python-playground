4dcde98553fb8f233d5ba9dd9b737ce9f2777330
Hwanee <khh0508@gmail.com>
2026-03-17 22:36:57 +0900

    day1 study

---
+++ week1/Day1_print.py
@@ -0,0 +1,31 @@
+# #Day1 Python 첫 출력, 변수, 자료형
+
+# #print("Hello World!")
+# #print("안녕하세요!")
+# #print(1234567890)
+# #print(3.141592)
+
+# #name="화니"
+# #NAME="비니"
+# #age=30
+# #AGE=20
+# #print(name+str(age))
+# #print(NAME, AGE)
+
+name="화늬"
+age=40
+hobby = "게임"
+is_worker = True
+
+print("제 이름은", name + "입니다.")
+print("나이는", str(age) + "살입니다.")
+print("취미는", hobby + "입니다.")
+print("저는 직장인입니다.", is_worker)
+
+print(type(name))
+print(type(age))
+print(type(hobby))
+print(type(is_worker))
+print(type(3.14))
+
+
