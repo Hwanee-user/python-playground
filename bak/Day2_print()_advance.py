3c5d7f38bf7502deb4ba9e5c554fa847bd007083
Hwanee <khh0508@gmail.com>
2026-03-17 19:34:23 +0900

    커리큘럼 변경(진도 압축화)

--- bak/Day2_print()_advance.py
+++ bak/Day2_print()_advance.py
@@ -8,6 +8,8 @@ print("나이:"+str(AGE)+" 살입니다.")
 print("나이:",str(AGE+10),"살입니다.")
 print("나이:",str(AGE+20),"살입니다.")
 print("숫자를 선택하세요.", "1.", 20, "2.", 30, "3.", 40)
+
+AGE=30
 print("내 나이 :", AGE)
 print("내 나이 :", AGE+10)
 print("내 나이 :", AGE+20)
@@ -23,5 +25,7 @@ this_year=2026
 print("올해는", this_year,"년입니다.")
 print("올해는", str(this_year)+"년입니다."
       "내년은", str(this_year+1)+"년입니다."
-      "작년은", str(this_year-1)+"년입니다."
-)
+      "작년은", str(this_year-1)+"년입니다.")
+
+print(this_year >= 2025)
+print("올해는" + str(this_year) + "년입니다. 올해는 2025년 이상인가요?", str(this_year >= 2025))
